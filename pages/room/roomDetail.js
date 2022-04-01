c5f488211f7b70b09ba01c5e780a5871643d1018
sbx0 <782715416@qq.com>
2022-04-01 21:37:40 +0800

    improve user experience

--- pages/room/roomDetail.js
+++ pages/room/roomDetail.js
@@ -77,6 +77,7 @@ export default function RoomDetail({
                                 }}
                                 onSuccess={() => {
                                     roomInfo.setFlag(!roomInfo.flag);
+                                    setIsIAmIn(!isIAmIn);
                                 }}
                             />
                             :
