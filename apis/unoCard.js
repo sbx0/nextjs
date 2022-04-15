f4435ef9e1d18f3b9781fa56c19702aaf8ab7f8c
sbx0 <782715416@qq.com>
2022-04-15 11:36:57 +0800

    fix playCards no choose instance

--- apis/unoCard.js
+++ apis/unoCard.js
@@ -2,5 +2,5 @@ import {get} from '../utils/http'
 
 export const drawCard = (params, cookies, additionalHeaders) => get('/UNO/uno/card/draw/' + params.roomCode, null, cookies, additionalHeaders);
 export const myCards = (params, cookies) => get('/UNO/uno/card/my/' + params.roomCode, null, cookies);
-export const playCards = (params, cookies) => get('/UNO/uno/card/play/' + params.roomCode + '/' + params.uuid, {color: params.color}, cookies);
+export const playCards = (params, cookies, additionalHeaders) => get('/UNO/uno/card/play/' + params.roomCode + '/' + params.uuid, {color: params.color}, cookies, additionalHeaders);
 export const discardCards = (params, cookies) => get('/UNO/uno/card/discard/' + params.roomCode, null, cookies);
