c4e49367f9be8d9bb0c41b63d07d9d64fbcfb55c
sbx0 <782715416@qq.com>
2022-05-24 10:38:52 +0800

    add room result page

--- pages/room/components/roomResult.js
+++ pages/room/components/roomResult.js
@@ -2,6 +2,7 @@ import React, {useContext, useEffect, useState} from "react";
 import {SSEContext} from "./roomSSE";
 import {listByGameRoom} from "../../../apis/unoResult";
 import {useRouter} from "next/router";
+import styles from './roomResult.module.css';
 
 export default function RoomResult() {
     const router = useRouter()
@@ -16,16 +17,36 @@ export default function RoomResult() {
         }))
     }, [])
 
-    return <>
-        {
-            data?.map((u) => {
-                return <p key={u.id}>{u.userName} {u.ranking}</p>
-            })
-        }
-        <button onClick={() => {
+    return <div className={styles.container}>
+        <h1 className={styles.title}>排行榜</h1>
+        <GameResultUsers users={data}/>
+        <button className={styles.button} onClick={() => {
             router.push("/").then(r => r)
         }}>
             再来一局
         </button>
+    </div>
+}
+
+function GameResultUsers({users}) {
+    return <div className={styles.content}>
+        <ol className={styles.orderList}>
+            {
+                users?.map((u) => {
+                    return <li key={u.id} className={styles.listItem}>
+                        <GameResultUser user={u}/>
+                    </li>
+                })
+            }
+        </ol>
+    </div>
+}
+
+function GameResultUser({user}) {
+    return <>
+        <p>
+            <strong>{user?.userName}</strong>
+            <span>{user?.ranking}</span>
+        </p>
     </>
 }
